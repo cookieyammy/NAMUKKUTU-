56be3be2ddd5fae21fb838def4b49b4e56d2c56c
cookieyammy <161109056+cookieyammy@users.noreply.github.com>
2024-03-18 18:56:45 +0900

    나무끄투 최신 업데이트
    NamuKKuTu 최신 업데이트

---
+++ server-delete.bat
@@ -0,0 +1,7 @@
+del "Server\run.bat"
+del "Server\lib\Web\lib\in_game_kkutu.js"
+
+rmdir /S /Q "./Server/node_modules"
+rmdir /S /Q "./Server/lib/node_modules"
+rmdir /S /Q "./Server/lib/etc"
+// 서버를 삭제하는 파일입니다. 신중히 선택 후 파일을 실행하세요. 삭제할 시 모든 주 데이터가 사라집니다.
